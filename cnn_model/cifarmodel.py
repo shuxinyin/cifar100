1e8eed4c47e6d0207cd2dc8e868475f03595b3f7
shuxinyin <844040010@qq.com>
2021-07-03 10:51:17 +0800

    add another method of constructing cnn model

--- cnn_model/cifarmodel.py
+++ cnn_model/cifarmodel.py
@@ -30,30 +30,29 @@ class Cifar100Net(nn.Module):
         return x
 
 
+# another method construct a cnn model
 class Cifar100Net2(nn.Module):
     def __init__(self):
         super(Cifar100Net2, self).__init__()
-
         # conv output size: [(width - kernel + 2padding)/stride]+1  32-3+2+1=32
-        self.conv0 = nn.Conv2d(3, 64, 3, padding=1),  # 输入通道数为3，输出通道数为64， filter=3*3
-        self.batch_norm0 = nn.BatchNorm2d(64),
-        self.relu0 = nn.ReLU(),
-        self.conv1 = nn.Conv2d(64, 128, 3, padding=1),
-        self.maxpool = nn.MaxPool2d(2, 2),  # (32-2+0)/2+1=16
-        self.batch_norm1 = nn.BatchNorm2d(128),
-        self.relu1 = nn.ReLU()
+        self.conv1 = nn.Conv2d(3, 64, 3, padding=1)  # 输入通道数为3，输出通道数为64， filter=3*3
+        self.batch_norm0 = nn.BatchNorm2d(64)
+        self.conv1 = nn.Conv2d(64, 128, 3, padding=1)
+        self.maxpool = nn.MaxPool2d(2, 2)  # (32-2+0)/2+1=16
+        self.batch_norm1 = nn.BatchNorm2d(128)
 
         self.fc1 = nn.Linear(16 * 16 * 128, 1024)  # flatten length * width * channels
         self.drop1 = nn.Dropout2d()
         self.fc2 = nn.Linear(1024, 128)
 
     def forward(self, x):
-        net_x = self.net(x)
-        x = net_x.view(x.size(0), -1)
+        x = F.relu(self.batch_norm0(self.conv1(x)))
+        x = self.maxpool(F.relu(self.batch_norm1(self.conv2(x))))
+        x = torch.flatten(x.size(0), -1)  # flatten all dimensions except batch
+
         x = F.relu(self.fc1(x))
         x = self.drop1(x)
         x = self.fc2(x)
-
         return x
 
 
